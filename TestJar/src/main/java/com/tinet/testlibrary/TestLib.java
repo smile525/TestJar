fa97f4050f498b349bfe2e533c3cb4619d2e464c
smile525 <zp045217@163.com>
2022-04-08 17:11:13 +0800

    第一次提交

---
+++ TestJar/src/main/java/com/tinet/testlibrary/TestLib.java
@@ -0,0 +1,16 @@
+package com.tinet.testlibrary;
+
+import android.os.Build;
+
+/**
+ * @ProjectName: TestJar
+ * @ClassName: TestLib
+ * @Author: zhangping
+ * @CreateDate: 2022/4/8 11:15
+ * @Description: 描述说明
+ */
+public class TestLib {
+//    public static String getVersion() {
+//        return Build.VERSION.CODENAME;
+//    }
+}
---
+++ app/src/main/java/com/tinet/testjar/MainActivity.java
@@ -0,0 +1,17 @@
+package com.tinet.testjar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tinet.testlibrary.TestLib;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
